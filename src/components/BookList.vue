ffc8bbe54ee71b82ab159f99609d059758b56749
Hzunax <stijn.van.asschodt@gmail.com>
2018-11-26 13:50:58 +0100

    Completed module 5 (Creating a BookForm component)

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,16 +3,19 @@
         <h1>{{ title }}</h1>
         <ul>
             <book-item v-for="book in books" :book='book'></book-item>
+            <book-form @addBook='appendBook'></book-form>
         </ul>
     </div>
 </template>
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
     name: 'BookList',
     components: {
         BookItem,
+        BookForm,
     },
     data() {
         return {
@@ -24,6 +27,11 @@ export default {
             ],
         };
     },
+    methods: {
+        appendBook(bookTitle, bookAuthor) {
+            this.books.push({ title: bookTitle, author: bookAuthor });
+        }
+    }
 }
 </script>
 <style scoped>
